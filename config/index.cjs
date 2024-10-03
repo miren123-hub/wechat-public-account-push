4b9ec96d6cdac2e1055465ef2d4a938a053d0dba
miren123-hub <3202360397@qq.com>
2024-10-03 20:39:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,7 +52,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'uhfLg9IN1AFR3kcetusuC0W6nTMi-1Jc7FUypK81qdk',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
